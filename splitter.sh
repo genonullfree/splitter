dbdeb70347221df457e0610d7b12b9aa859995d0
Stephen Halwes <shalwes@pretalen.com>
2015-05-08 21:59:35 -0400

    updated splitter to allow duration specification, and made the output quieter

--- splitter.sh
+++ splitter.sh
@@ -6,14 +6,28 @@
 #list=( *mp3 ); for i in "${list[@]}"; do sh splitter.sh "$i"; done
 #
 
-rm -rf "$1-split"
-mkdir "$1-split"
+if [ $1 ]; then
+    file_name=$1
+else
+    echo "You must provide the mp3 to be split as the argument"
+fi
+
+# the second argument is the time duration of the mp3 segments
+# it can be given in seconds or hh:mm:ss format
+if [ $2 ]; then
+    duration=$2
+else
+    duration=1800
+fi
+
+rm -rf "$file_name-split"
+mkdir "$file_name-split"
 count=0;
 
-while( [[ $(ffmpeg -i "$1" -acodec copy -t 1800 -ss $((1800*count)) "$1-split"/"$count-$1")==0 ]] ); do
-echo "ffmpeg -i "$1" -acodec copy -t 1800 -ss $((1800*count)) "$1-split"/"$count-$1""
-    if [[ $(wc -c "$1-split"/"$count-$1"|cut -f 1 -d ' ') -le 550 ]]; then
-        rm "$1-split"/"$count-$1"
+while( [[ $(ffmpeg -i "$file_name" -acodec copy -t $duration -ss $(($duration*count)) "$file_name-split"/"$count-$file_name" &> /dev/null)==0 ]] ); do
+echo "ffmpeg -i "$file_name" -acodec copy -t $duration -ss $(($duration*count)) "$file_name-split"/"$count-$file_name""
+    if [[ $(wc -c "$file_name-split"/"$count-$file_name"|cut -f 1 -d ' ') -le 550 ]]; then
+        rm "$file_name-split"/"$count-$file_name"
         exit
     fi  
     count=$((count+1))
